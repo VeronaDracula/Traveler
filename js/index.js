f5e38b7f722093b278de1ad5a7fb0403df972376
VeronaDracula <45635800+VeronaDracula@users.noreply.github.com>
2020-09-16 13:06:35 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    var owl=$(".owl-carousel");
+    owl.owlCarousel({
+
+        responsive:{
+            0:{
+                items: 2
+            },
+            991:{
+                items: 4
+            }
+        }
+    });
+
+
+});
